38ccea4ecb04c893d631473a5bf4e71132825e7b
Gruby <coreywilliam21@outlook.com>
2018-05-08 18:08:22 +1000

    Initial file commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruteForceMedian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {-5, -1, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
+            int median = Median(array);
+            Console.WriteLine(median);
+        }
+        /// <summary>
+        /// Returns the median value in a given array A of n numbers. 
+        /// This is the kth element, where k = n/2, if the array was sorted.
+        /// </summary>
+        static int BruteForceMedian(int[] array)
+        {
+            int operations = 0;
+            int k = (int)array.Length / 2;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int a = 0; a <= array.Length - 1; a++)
+            {
+                int numSmaller = 0;
+                int numEqual = 0;
+                for (int b = 0; b <= array.Length - 1; b++)
+                {
+                    operations += 1;
+                    if (array[b] < array[a]) numSmaller += 1;
+                    else
+                    {
+                        if (array[b] == array[a]) numEqual += 1;
+                    }
+                }
+                if (numSmaller < k && k <= (numSmaller + numEqual)) return array[a];
+            }
+            watch.Stop();
+            return 0; // Haven't implemented operations.
+        }
+        /// <summary>
+        /// Returns the median value in a given array A of n numbers.
+        /// </summary>
+        static int Median(int[] array)
+        {
+            int length = array.Length;
+            if (length == 1) return array[0];
+            else return Select(array, 0, (int)length / 2, length - 1);
+        }
+        /// <summary>
+        /// Returns the value at index m in array slice A[l..h], if the slice 
+        /// were sorted into nondecreasing order.
+        /// </summary>
+        static int Select(int[] array, int l, int m, int h)
+        {
+            int operations = 0;
+            int pos = Partition(array, l, h);
+            if (pos == m) return array[pos];
+            if (pos > m) return Select(array, l, m, pos - 1);
+            if (pos < m) return Select(array, pos + 1, m, h);
+            return 0; // Haven't implemented operations.
+        }
+        /// <summary>
+        /// Partitions array slic A[l..h] by moving element A[l} to the position
+        /// it would have if the array slice was sorted, and by moving all
+        /// values in the slice smaller than A[l] to earlier positions, and all values 
+        /// larger than ore
+        ///
+        /// </summary>
+        static int Partition(int[] array, int l, int h)
+        {
+            int pivotal = array[l];
+            int pivotloc = l;
+            for (int a = l + 1; a < h; a++)
+            {
+                if (array[a] < pivotal)
+                {
+                    pivotloc += 1;
+                    Swap(array, pivotloc, a); // Swap elements around pivot
+                }
+            }
+            Swap(array, l, pivotloc); // Put pivot element in place.
+            return pivotloc;
+        }
+        /// <summary>
+        /// Swaps a value a in an array with value b.
+        /// </summary>
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+    }
+}
